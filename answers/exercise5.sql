8c014b22e928d21b53c3147046df8f2611670307
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-15 23:47:45 -0500

    done IS exercise

---
+++ answers/exercise5.sql
@@ -0,0 +1,2 @@
+SELECT * FROM students
+WHERE country IN 'Sint Maarten' OR country IN 'Haiti';
---
+++ answers/exercise6.sql
@@ -0,0 +1,2 @@
+SELECT * FROM students
+WHERE country NOT IN 'Sint Maarten' OR country NOT IN 'Haiti';
